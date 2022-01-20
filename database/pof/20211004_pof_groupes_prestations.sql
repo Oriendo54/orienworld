090fddaab319e2527eb638ff51cafef0cf4ff72c
J. Ambroise <79835109+JAmbroise@users.noreply.github.com>
2022-01-20 17:22:54 +0100

    database

--- database/pof/20211004_pof_groupes_prestations.sql
+++
@@ -1,7 +0,0 @@
-/**
- * Author:  julienambroise
- * Created: 04/10/2021
-**/
-
-ALTER TABLE `pof_prestation_groupe_lien`
-ADD `id_tarif_defaut` int(10);
--- database/pof/20211007_pof_cheval.sql
+++
@@ -1,8 +0,0 @@
-/**
- * Author:  julienambroise
- * Created: 04/10/2021
-**/
-
-ALTER TABLE `pof_cheval`
-ADD `actif` int(2) default 0
-AFTER `id_cheval_statut`;
